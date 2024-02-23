a1b703e1ed90e6265a94b5d74306a30b5f5630df
Tyrrell <50052035+TyrrellDMB@users.noreply.github.com>
2024-02-23 16:56:59 -0500

    Update site.config.js
    Updated file to reflect new website

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "CyberAdvent",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    role: "Cybersecurity Blog",
+    bio: "Making security more digestable.",
+    email: "cyberadvent.com@gmail.com",
     linkedin: "morethanmin",
     github: "morethanmin",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `CyberAdvent`,
+      href: "https://www.youtube.com/@CyberAdvent",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Cyber Advent Blog",
+    description: "Welcome to Cyber Advent",
   },
 
   // CONFIG configration (required)
@@ -53,7 +53,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
