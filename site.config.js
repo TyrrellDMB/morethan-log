72398e395e272e0becae182b347ed5760580fb3f
Tyrrell <50052035+TyrrellDMB@users.noreply.github.com>
2024-02-23 23:24:37 -0500

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "Tyrrell B.",
+    name: "Tyrrell B",
     image: "/cyberadvent_logo_black.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Cybersecurity Professional",
     bio: "BSCIA, CASP+, PJPT, SSCP",
